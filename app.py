ceec7e6406870275db2e1d95d9eb1602d7c90283
Atharva Dumbre <atharva.dumbre1@gmail.com>
2024-09-29 08:29:38 -0400

    revert

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def analyze_review_route():
     return jsonify({"result": result})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=False, host='0.0.0.0')
